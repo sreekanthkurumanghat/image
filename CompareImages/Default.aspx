afc6ebeb223336900ee831e189d58f74a9047b62
sreekanthkurumanghat <sreekanth.kurumanghat@gmail.com>
2015-07-14 21:09:24 +0530

    image
    compare two images

---
+++ CompareImages/Default.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Untitled Page</title>
+    <style type="text/css">
+        .style1
+        {
+            width: 105%;
+            height: 245px;
+        }
+        .style2
+        {
+            font-weight: normal;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server" enctype="multipart/form-data">
+    <div>
+    
+        <asp:Panel ID="Panel1" runat="server" Font-Bold="True" 
+            GroupingText="COMPARING IMAGES IN ASP.NET" Height="132px" Width="440px">
+            <table class="style1">
+                <tr>
+                    <td class="style2">
+                        SELECT IMAGE ONE</td>
+                    <td>
+                        <asp:FileUpload ID="FileUpload1" runat="server" />
+                    </td>
+                    <td>
+                        <asp:Image ID="Image1" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style2">
+                        SELECT IMAGE TWO</td>
+                    <td>
+                        <asp:FileUpload ID="FileUpload2" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style2">
+                        &nbsp;</td>
+                    <td>
+                        <asp:Button ID="btncompare" runat="server" Font-Bold="True" 
+                            onclick="btncompare_Click" Text="Compare" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style2">
+                        <asp:Label ID="Label1" runat="server" Text="Label" Visible="False" 
+                            style="font-weight: 700"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="Label" Visible="False"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style2">
+                        <asp:Label ID="Label3" runat="server" style="font-weight: 700" Text="Label" 
+                            Visible="False"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:Label ID="Label4" runat="server" style="font-weight: 700" Text="Label" 
+                            Visible="False"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </asp:Panel>
+    
+    </div>
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ CompareImages/Default.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+//using System.Web.UI.WebControls;
+//using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using System.IO;
+using System.Drawing.Imaging; 
+public partial class _Default : System.Web.UI.Page
+{
+    //Byte Array Declaration
+    byte[] _barray1;
+    byte[] _barray2;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+        }
+    }
+    protected void btncompare_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            //Reading Bytes From Uploaded Images
+
+            if (FileUpload1.HasFile && FileUpload2.HasFile)
+            {
+                using (BinaryReader reader1 = new BinaryReader(FileUpload1.PostedFile.InputStream))
+                {
+                    using (BinaryReader reader2 = new BinaryReader(FileUpload2.PostedFile.InputStream))
+                    {
+                        _barray1 = reader1.ReadBytes(FileUpload1.PostedFile.ContentLength);
+                        _barray2 = reader2.ReadBytes(FileUpload2.PostedFile.ContentLength);
+                    }
+                }
+            }
+            //Converting Byte Array To Image And Then Into Bitmap
+            ImageConverter ic = new ImageConverter();
+            Image img = (Image)ic.ConvertFrom(_barray1);
+            Bitmap bmp1 = new Bitmap(img);
+            
+            Image img1 = (Image)ic.ConvertFrom(_barray2);
+            Bitmap bmp2 = new Bitmap(img1);
+
+            //Calling Compare Function
+
+            if (Class1.Compare(bmp1, bmp2) == Class1.CompareResult.ciCompareOk)
+            {
+                Label1.Visible = true;
+                Label1.Text = "Images Are Same";
+            }
+           if (Class1.Compare(bmp1, bmp2) == Class1.CompareResult.ciPixelMismatch)
+            {
+                Label2.Visible = true;
+                Label2.Text = "Pixel not Matching";
+            }
+            if (Class1.Compare(bmp1, bmp2) == Class1.CompareResult.ciSizeMismatch)
+            {
+                Label3.Visible = true;
+                Label3.Text = "Size is not Same";
+            }
+            if (Class1.Compare(bmp1, bmp2) != Class1.CompareResult.ciCompareOk)
+            {
+                Label1.Visible=true;
+                Label1.Text = "images are not same";
+            }
+
+            if (Class1.Compare(bmp1, bmp2) != Class1.CompareResult.ciPixelMismatch )
+            {
+                Label2.Visible = true;
+                Label2.Text = "Pixel Matching";
+            }
+            if (Class1.Compare(bmp1, bmp2) != Class1.CompareResult.ciSizeMismatch)
+            {
+                Label3.Visible = true;
+                Label3.Text = "Size is Same";
+            }
+
+
+        }
+        catch (Exception ex)
+        {
+            Label1.Visible = true;
+            Label1.Text = ex.Message.ToString();
+        }
+    }
+    
+}
